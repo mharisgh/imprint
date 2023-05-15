5f2cd0273bb2d05707bd2c28bcf748c86a559771
Haris <hellomharis@gmail.com>
2023-05-15 19:00:01 +0530

    images

--- src/main.js
+++ src/main.js
@@ -2,6 +2,5 @@ const button = document.querySelector('#menu-button');
 const menu = document.querySelector('#menu');
 
 
-button.addEventListener('click', () => {
-  menu.classList.toggle('hidden');
-});
+gsap.registerPlugin(ScrollTrigger);
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        accent: "#DAFF8C",
+      }
+    },
   },
   plugins: [],
 }
